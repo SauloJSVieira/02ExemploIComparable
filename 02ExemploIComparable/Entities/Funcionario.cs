4d25bff7ebfd6a279d15bac0772c8afee69320c0
saulojsvieira <saulo.vieira@ufms.br>
2022-12-16 13:07:24 -0400

    Adicionar arquivos de projeto.

---
+++ 02ExemploIComparable/Entities/Funcionario.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02ExemploIComparable.Entities
+{
+    internal class Funcionario : IComparable
+    {
+
+        public string Nome { get; set; }
+        public double Salario { get; set; }
+        public Funcionario(string csvFuncionario)
+        {
+            string[] vetor = csvFuncionario.Split(',');
+            Nome = vetor[0];
+            Salario = double.Parse(vetor[1], CultureInfo.InvariantCulture);
+        }
+        public override string ToString()
+        {
+            return Nome + ", " + Salario.ToString("f2", CultureInfo.InvariantCulture);
+        }
+        
+        public int CompareTo(object obj)
+        {
+            if (!(obj is Funcionario))
+            {
+                throw new NotImplementedException("\nErro na comparação!\nO argumento não é do tipo Funcionário.");
+            }
+
+            Funcionario outroFuncionario = obj as Funcionario;
+
+            return Salario.CompareTo(outroFuncionario.Salario);
+        }
+    }    
+}
---
+++ 02ExemploIComparable/Program.cs
@@ -0,0 +1,36 @@
+﻿using _02ExemploIComparable.Entities;
+
+namespace _02ExemploIComparable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string caminhoOrigem = @"c:\tmp\entradaDados.csv";
+
+            try
+            {
+                using (StreamReader leitor = File.OpenText(caminhoOrigem))
+                {
+                    List<Funcionario> lista = new List<Funcionario>();
+
+                    while (!leitor.EndOfStream)
+                    {
+                        lista.Add(new Funcionario(leitor.ReadLine()));
+                    }
+
+                    lista.Sort();
+
+                    foreach (var item in lista)
+                    {
+                        Console.WriteLine(item);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("\nOcurreu um erro!\n" + e.Message);
+            }
+        }
+    }
+}
